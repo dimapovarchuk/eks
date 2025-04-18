b474339b3ea0d667dc135e8d53003abf89552ca3
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2025-04-18 20:04:53 +0300

    Added changes

--- project/terraform/backend.tf
+++ project/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dima001-terraform-state-bucket"
+    bucket         = "dima001"
     key            = "monitoring/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
